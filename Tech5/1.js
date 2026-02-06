096695bcc77c2f7ef297be6d69d771fad10186ff
shizukuwife <wunanimationmeme@gmail.com>
2026-02-06 13:36:49 +0700

    Add files via upload

---
+++ Tech5/1.js
@@ -0,0 +1,52 @@
+//1.
+let num1 = 100;
+let num2 = 2.5;
+let char1 = 'A';
+let char2 = 'B';
+let str1 = 'C5';
+
+console.log(num1);
+console.log(num2);
+console.log(char1);
+console.log(char2);
+console.log(str1);
+
+//2.
+const val1 = 100;
+const val2 = 2.5;
+const val3 = 'A';
+const val4 = 'B';
+const val5 = 'C5';
+
+console.log(val1, val2);
+console.log(val3, val4);
+console.log(val5);
+
+//3.
+const n1 = 100;
+const n2 = 2.5;
+const char1 = 'A';
+const char2 = 'B';
+const str1 = 'C5';
+
+console.log(`${n1}${n2}`, `${char1}${char2}`, str1);
+
+//4.
+const val1 = 100;
+const val2 = 2.5;
+const val3 = 'A';
+const val4 = 'B';
+const val5 = 'C5';
+
+console.log(val1 + "Hello");
+console.log(val2 + "Hello");
+console.log("Hello" + val3);
+
+//5.
+const v1 = 100;
+const v2 = 2.5;
+const v3 = 'A';
+const v4 = 'B';
+const v5 = 'C5';
+
+console.log(v1 + "Hello" + v2 + "Hello" + v3);
